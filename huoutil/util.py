0ff4dc9f3ec4b83c0e326c6a70c5e0d4b862fca2
huohuarong <huohuarong@baidu.com>
2016-12-14 16:40:07 +0800

    fix bug

--- huoutil/util.py
+++ huoutil/util.py
@@ -798,7 +798,7 @@ def update_dict(d, u):
     """
     for k, v in u.iteritems():
         if isinstance(v, collections.Mapping):
-            r = update(d.get(k, {}), v)
+            r = update_dict(d.get(k, {}), v)
             d[k] = r
         else:
             d[k] = u[k]
