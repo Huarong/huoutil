3131cd104fa8f8ee93e89c9db8f78a3c00c0d831
Huarong <huohuarong@gmail.com>
2017-06-13 18:04:05 +0800

    format all python source with yapf

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from distutils.core import setup, Extension
 import huoutil
 import os
 
-
-setup(name='huoutil',
+setup(
+    name='huoutil',
     version=huoutil.__version__,
     description=huoutil.__description__,
     author=huoutil.__author__,
     author_email=huoutil.__author_email__,
-    packages=['huoutil'],
-    )
+    packages=['huoutil'], )
