1249cd2c30aa3fa7eceb5eec487cbec5d34f359a
huohuarong <huohuarong@zuoshouyisheng.com>
2021-05-14 19:25:07 +0800

    __author_email__ is required

--- huoutil/__init__.py
+++ huoutil/__init__.py
@@ -1,6 +1,7 @@
 __version__ = '1.3.1'
 __description__ = 'util module of Huarong Huo'
 __author__ = 'Huarong Huo'
+__author_email__ = 'huohuarong@gmail.com'
 
 from .util import *
 from .cache import *
