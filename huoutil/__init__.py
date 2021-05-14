7e2832b153e6b072ae4a15d93d91712eced8bf0d
Balzac <YTkks001@163.com>
2021-05-14 19:10:31 +0800

    update version

--- huoutil/__init__.py
+++ huoutil/__init__.py
@@ -1,7 +1,6 @@
-__version__ = '1.3'
+__version__ = '1.3.1'
 __description__ = 'util module of Huarong Huo'
 __author__ = 'Huarong Huo'
-__author_email__ = 'huohuarong@baidu.com'
 
 from .util import *
 from .cache import *
